1d036951082001951b3ee86041b4396dcd29b55b
Amirhossein Ghanipour <d3v1ll3n@gmail.com>
2025-08-20 07:34:58 +0330

    refactor user model and profile header widget

---
+++ lib/models/user.dart
@@ -0,0 +1,25 @@
+class User {
+  final String uid;
+  final String name;
+  final String email;
+  final String package;
+  final String expiryDate;
+
+  const User({
+    required this.uid,
+    required this.name,
+    required this.email,
+    required this.package,
+    required this.expiryDate,
+  });
+
+  factory User.demo() {
+    return const User(
+      uid: 'EQE13255',
+      name: 'Martin Hamilton',
+      email: 'Martin.Hammilton1177@gmail.com',
+      package: 'Dolphin',
+      expiryDate: '30 November 2025',
+    );
+  }
+} 
---
+++ lib/widgets/profile_header.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../constants/app_colors.dart';
+import '../constants/app_dimensions.dart';
+import '../constants/app_text_styles.dart';
+import '../constants/app_icons.dart';
+
+class ProfileHeader extends StatelessWidget {
+  const ProfileHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        color: AppColors.cardBackground,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(AppDimensions.borderRadiusXXL),
+          bottomRight: Radius.circular(AppDimensions.borderRadiusXXL),
+        ),
+      ),
+      padding: const EdgeInsets.only(
+        top: 48,
+        left: AppDimensions.spacingXL,
+        right: 18,
+        bottom: 13,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            AppIcons.logo,
+            width: AppDimensions.iconSizeXXL,
+            height: AppDimensions.iconSizeXXL,
+          ),
+          const Text("Profile", style: AppTextStyles.headingLarge),
+          _buildSearchButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSearchButton() {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(AppDimensions.borderRadiusL),
+        splashColor: AppColors.overlayLight.withOpacity(0.3),
+        highlightColor: AppColors.overlayLight.withOpacity(0.2),
+        child: Padding(
+          padding: const EdgeInsets.all(AppDimensions.spacingS),
+          child: Image.asset(
+            AppIcons.magnifier,
+            width: AppDimensions.iconSizeL,
+            height: AppDimensions.iconSizeL,
+          ),
+        ),
+      ),
+    );
+  }
+} 
